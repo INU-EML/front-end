ee78fc827f6a47a22a9b84258672361f0834151d
Sung Hoyeong <hbman01@naver.com>
2026-03-19 01:17:45 +0900

    News 논문 정보 최신화

--- src/data/News/newsData.js
+++ src/data/News/newsData.js
@@ -1,39 +1,63 @@
 // News data
 export const newsList = [
   {
-    id: 1,
+    id: 28,
+    date: "2026.02.02",
+    title: "Won's paper has been accepted to  Applied Catalysis B: Environment",
+    image: "/images/News/Wons_Operando dynamic self-assembly.png",
+    featured: true,
+    category: "publication"
+  },
+  {
+    id: 27,
+    date: "2025.10.27",
+    title: "Jang’s paper has been accepted to Chemical Engineering Journal.",
+    image: "/images/News/Design of exsolved on-cell catalyst for high-performance direct ammonia solid oxide fuel cells.png",
+    featured: true,
+    category: "publication"
+  },
+  {
+    id: 26,
+    date: "2025.07.09",
+    title: "Park’s paper has been accepted to International Journal of Energy Research.",
+    image: "/images/News/Parks_Tailoring Oxygen Vacancy.png",
+    featured: true,
+    category: "publication"
+  },
+  {
+    id: 25,
     date: "2025.03.15",
-    title: "Won’s paper has been accepted to Chemical Engineering Journal.",
-    image: "/images/News/Won’s paper has been accepted to Chemical Engineering Journal..png",
+    title: "Won's paper has been accepted to Chemical Engineering Journal.",
+    image: "/images/News/Won's paper has been accepted to Chemical Engineering Journal..png",
     featured: true,
     category: "publication"
   },
   {
-    id: 2,
+    id: 24,
     date: "2025.02.11",
-    title: "Jeong’s paper has been accepted to International Journal of Energy Research.",
-    image: "/images/News/Jeong’s paper has been accepted to International Journal of Energy Research. .png",
+    title: "Jeong's paper has been accepted to International Journal of Energy Research.",
+    image: "/images/News/Jeong's paper has been accepted to International Journal of Energy Research. .png",
     featured: true,
     category: "publication"
   },
   {
-    id: 3,
+    id: 23,
     date: "2024.08.28",
-    title: "Jeon’s paper has been accepted to Small.",
-    image: "/images/News/Jeon’s paper has been accepted to Small. .png",
+    title: "Jeon's paper has been accepted to Small.",
+    image: "/images/News/Jeon's paper has been accepted to Small. .png",
     featured: true,
     category: "publication"
   },
   {
-    id: 4,
+    id: 22,
     date: "2024.08.27",
-    title: "Jeong’s paper has been accepted to Solid State Ionics.",
-    image: "/images/News/Jeong’s paper has been accepted to Solid State Ionics. .png",
+    title: "Jeong's paper has been accepted to Solid State Ionics.",
+    image: "/images/News/Jeong's paper has been accepted to Solid State Ionics. .png",
     featured: true,
     category: "publication"
   },
   {
-    id: 5,
+    id: 21,
     date: "2024.04.30",
     title: "DaHyun Han was awarded the Best Poster Award at the 2024 Spring Meeting of the Korean Ceramic Society. Congratulations!",
     image: "/images/News/DaHyun Han was awarded the Best Poster Award at the 2024 Spring Meeting of the Korean Ceramic Society. Congratulations! .jpg",
@@ -41,7 +65,7 @@ export const newsList = [
     category: "award"
   },
   {
-    id: 6,
+    id: 20,
     date: "2024.04.19",
     title: "Yo Han Kim was awarded the Best Oral Presentation Award at the 2024 Spring Conference of the Korean Hydrogen & New Energy Society. Congratulations!",
     image: "/images/News/Yo Han Kim was awarded the Best Oral Presentation Award at the 2024 Spring Conference of the Korean Hydrogen & New Energy Society. Congratulations! .jpg",
@@ -49,23 +73,23 @@ export const newsList = [
     category: "award"
   },
   {
-    id: 7,
+    id: 19,
     date: "2024.04.19",
-    title: "Jeong’s paper has been accepted to Applied Surface Science.",
-    image: "/images/News/Jeong’s paper has been accepted to Applied Surface Science. .png",
+    title: "Jeong's paper has been accepted to Applied Surface Science.",
+    image: "/images/News/Jeong's paper has been accepted to Applied Surface Science. .png",
     featured: true,
     category: "publication"
   },
   {
-    id: 8,
+    id: 18,
     date: "2024.04.11",
-    title: "Kim’s paper has been accepted to Journal of the European Ceramic Society.",
-    image: "/images/News/Kim’s paper has been accepted to Journal of the European Ceramic Society. .png",
+    title: "Kim's paper has been accepted to Journal of the European Ceramic Society.",
+    image: "/images/News/Kim's paper has been accepted to Journal of the European Ceramic Society. .png",
     featured: true,
     category: "publication"
   },
   {
-    id: 9,
+    id: 17,
     date: "2023.11.02",
     title: "Professor Jae-ha Myung was awarded the Fuel Cell Young Researcher Award at the 2023 Fall Meeting & Academic Presentation of the Korean Electrochemical Society. Congratulations!",
     image: "/images/News/Professor Jae-ha Myung was awarded the Fuel Cell Young Researcher Award at the 2023 Fall Meeting & Academic Presentation of the Korean Electrochemical Society. Congratulations! .jpg",
@@ -73,7 +97,7 @@ export const newsList = [
     category: "award"
   },
   {
-    id: 10,
+    id: 16,
     date: "2023.10.20",
     title: "Bo-Ram Won was awarded the Best Oral Presentation Award at the 2023 Fall Meeting of the Korean Ceramic Society. Congratulations!",
     image: "/images/News/Bo-Ram Won was awarded the Best Oral Presentation Award at the 2023 Fall Meeting of the Korean Ceramic Society. Congratulations! .jpg",
@@ -81,7 +105,7 @@ export const newsList = [
     category: "award"
   },
   {
-    id: 11,
+    id: 15,
     date: "2023.10.20",
     title: "Hyejin Jeon was awarded the Best Poster Award at the 2023 Fall Meeting of the Korean Ceramic Society. Congratulations!",
     image: "/images/News/Hyejin Jeon was awarded the Best Poster Award at the 2023 Fall Meeting of the Korean Ceramic Society. Congratulations! .png",
@@ -89,7 +113,7 @@ export const newsList = [
     category: "award"
   },
   {
-    id: 12,
+    id: 14,
     date: "2023.10.19",
     title: "A review paper has been accepted to Nano-Micro Letters.",
     image: "/images/News/A review paper has been accepted to Nano-Micro Letters. .png",
@@ -105,7 +129,7 @@ export const newsList = [
     category: "award"
   },
   {
-    id: 14,
+    id: 12,
     date: "2023.09.22",
     title: "Hyeongwon Jeong was awarded the Best Poster Award at the 21st International Conference on Solid-State Protonic Conductors. Congratulations!",
     image: "/images/News/Hyeongwon Jeong was awarded the Best Poster Award at the 21st International Conference on Solid-State Protonic Conductors. Congratulations! .png",
@@ -113,23 +137,23 @@ export const newsList = [
     category: "award"
   },
   {
-    id: 15,
+    id: 11,
     date: "2023.08.02",
-    title: "Park’s paper has been accepted to Advanced Materials Technologies.",
-    image: "/images/News/Park’s paper has been accepted to Advanced Materials Technologies. .gif",
+    title: "Park's paper has been accepted to Advanced Materials Technologies.",
+    image: "/images/News/Park's paper has been accepted to Advanced Materials Technologies. .gif",
     featured: true,
     category: "publication"
   },
   {
-    id: 16,
+    id: 10,
     date: "2023.7.31",
-    title: "Park’s paper has been accepted to Ceramics International.",
-    image: "/images/News/Park’s paper has been accepted to Ceramics International. .gif",
+    title: "Park's paper has been accepted to Ceramics International.",
+    image: "/images/News/Park's paper has been accepted to Ceramics International. .gif",
     featured: true,
     category: "publication"
   },
   {
-    id: 17,
+    id: 9,
     date: "2023.4.13",
     title: "Professor Jae-ha Myung was awarded the Academic Progress Award from the Korean Ceramic Society in the 2023 Spring Symposium.",
     image: "/images/News/Professor Jae-ha Myung was awarded the Academic Progress Award from the Korean Ceramic Society in the 2023 Spring Symposium. .jpg",
@@ -138,63 +162,63 @@ export const newsList = [
     category: "award"
   },
   {
-    id: 18,
+    id: 8,
     date: "2023.3.30",
-    title: "Jeong’s paper has been accepted to Chemistry of Materials.",
-    image: "/images/News/Jeong’s paper has been accepted to Chemistry of Materials. .png",
+    title: "Jeong's paper has been accepted to Chemistry of Materials.",
+    image: "/images/News/Jeong's paper has been accepted to Chemistry of Materials. .png",
     featured: true,
     category: "publication"
   },
   {
-    id: 19,
+    id: 7,
     date: "2023.1.24",
-    title: "Kim’s paper has been accepted to Advanced Materials.",
-    image: "/images/News/Kim’s paper has been accepted to Advanced Materials. .gif",
+    title: "Kim's paper has been accepted to Advanced Materials.",
+    image: "/images/News/Kim's paper has been accepted to Advanced Materials. .gif",
     featured: true,
     category: "publication"
   },
   {
-    id: 20,
+    id: 6,
     date: "2022.10.01",
-    title: "Jo’s paper has been accepted to Applied Energy.",
-    image: "/images/News/Jo’s paper has been accepted to Applied Energy. .gif",
+    title: "Jo's paper has been accepted to Applied Energy.",
+    image: "/images/News/Jo's paper has been accepted to Applied Energy. .gif",
     featured: true,
     category: "publication"
   },
   {
-    id: 21,
+    id: 5,
     date: "2022.6.14",
-    title: "Ram’s paper has been accepted to Journal of the European Ceramic Society.",
-    image: "/images/News/Ram’s paper has been accepted to Journal of the European Ceramic Society. .gif",
+    title: "Ram's paper has been accepted to Journal of the European Ceramic Society.",
+    image: "/images/News/Ram's paper has been accepted to Journal of the European Ceramic Society. .gif",
     featured: true,
     category: "publication"
   },
   {
-    id: 22,
+    id: 4,
     date: "2022.3.28",
-    title: "Jeong’s paper has been accepted in ACS Applied Materials & Interface (Supplementary Cover)",
-    image: "/images/News/Jeong’s paper has been accepted in ACS Applied Materials & Interface (Supplementary Cover).jpg",
+    title: "Jeong's paper has been accepted in ACS Applied Materials & Interface (Supplementary Cover)",
+    image: "/images/News/Jeong's paper has been accepted in ACS Applied Materials & Interface (Supplementary Cover).jpg",
     featured: true,
     category: "publication"
   },
   {
-    id: 23,
+    id: 3,
     date: "2022.3.23",
-    title: "Kim’s paper has been accepted in Chemical Engineering Journal",
-    image: "/images/News/Kim’s paper has been accepted in Chemical Engineering Journal .gif",
+    title: "Kim's paper has been accepted in Chemical Engineering Journal",
+    image: "/images/News/Kim's paper has been accepted in Chemical Engineering Journal .gif",
     featured: true,
     category: "publication"
   },
   {
-    id: 24,
+    id: 2,
     date: "2020.12.10",
-    title: "Jo’s paper has been accepted in Applied Catalysis B: Environment",
-    image: "/images/News/Jo’s paper has been accepted in Applied Catalysis B- Environment .gif",
+    title: "Jo's paper has been accepted in Applied Catalysis B: Environment",
+    image: "/images/News/Jo's paper has been accepted in Applied Catalysis B- Environment .gif",
     featured: true,
     category: "publication"
   },
   {
-    id: 25,
+    id: 1,
     date: "2019.6.21",
     title: "Korea-UK-China symposium was held in INU.",
     image: "/images/News/Korea-UK-China symposium was held in INU. .jpg",
