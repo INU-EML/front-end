9cc908fee442c35bc910769fed17fbf3fd39ab0c
HoYeong Sung <hbman01@naver.com>
2026-08-09 22:32:56 +0900

    Current Member 현황 수정

--- src/data/Members/CurrentMembersData.js
+++ src/data/Members/CurrentMembersData.js
@@ -7,9 +7,13 @@ export const currentMembersData = {
       career: [
         "Ph.D.,Incheon National University, 2025",
         "M.S., Incheon National University, 2021",
-        "B.S., Incheon National University, 2019"
+        "B.S., Incheon National University, 2019",
       ],
-      researchFields: ["Exsolution", "Solid Oxide Cells", "Dry Reforming of Methane"]
+      researchFields: [
+        "Exsolution",
+        "Solid Oxide Cells",
+        "Dry Reforming of Methane",
+      ],
     },
   ],
   doctoralCourse: [
@@ -18,9 +22,14 @@ export const currentMembersData = {
       image: "/images/Memebers/CurrentMemebers/Doctor/Dr_Hyeongwon Jeong.jpg",
       career: [
         "Ph.D. Course at Incheon National University, 2021~",
-        "B.S., Incheon National University, 2021"
+        "B.S., Incheon National University, 2021",
       ],
-      researchFields: ["Exsolution", "Solid Oxide Cells", "Oxygen Kinetics Catalyst", "Ammonia Fueled SOFCs"]
+      researchFields: [
+        "Exsolution",
+        "Solid Oxide Cells",
+        "Oxygen Kinetics Catalyst",
+        "Ammonia Fueled SOFCs",
+      ],
     },
     {
       name: "Bo-Ram Won",
@@ -28,45 +37,49 @@ export const currentMembersData = {
       career: [
         "Ph.D. Course at Incheon National University, 2023~",
         "M.S., Incheon National University, 2023",
-        "B.S., Incheon National University, 2021"
+        "B.S., Incheon National University, 2021",
       ],
-      researchFields: ["Flexible Solid Oxide Cells", "Solid Oxide Electrolyte", "Nano catalysts"]
+      researchFields: [
+        "Flexible Solid Oxide Cells",
+        "Solid Oxide Electrolyte",
+        "Nano catalysts",
+      ],
     },
     {
       name: "Jong-Hyun Kim",
       image: "/images/Memebers/CurrentMemebers/Doctor/Dr_Jong Hyeon Kim.png",
       career: [
         "Ph.D. Course at Incheon National University, 2024~",
-        "B.S., Incheon National University, 2024"
+        "B.S., Incheon National University, 2024",
       ],
-      researchFields: ["Protonic Ceramic Fuel Cells"]
+      researchFields: ["Protonic Ceramic Fuel Cells"],
     },
     {
       name: "Wonjun Jang",
       image: "/images/Memebers/CurrentMemebers/Doctor/Dr_Wonjun Jang.jpg",
       career: [
         "Ph.D. Course at Incheon National University, 2025~",
-        "B.S., Incheon National University, 2025"
+        "B.S., Incheon National University, 2025",
       ],
-      researchFields: ["Ammonia Fueled Solid Oxide Fuel Cells"]
-    },    
+      researchFields: ["Ammonia Fueled Solid Oxide Fuel Cells"],
+    },
     {
       name: "Jun-Young Park",
       image: "/images/Memebers/CurrentMemebers/Doctor/Dr_Jun-Young Park.jpg",
       career: [
         "Ph.D. Course at Incheon National University, 2025~",
-        "B.S., Incheon National University, 2025"
+        "B.S., Incheon National University, 2025",
       ],
-      researchFields: ["Solid Oxide Fuel Cells"]
+      researchFields: ["Solid Oxide Fuel Cells"],
     },
     {
       name: "Hoyeong Sung",
       image: "",
       career: [
         "Ph.D. Course at Incheon National University, 2026~",
-        "B.S., Incheon National University, 2026"
-    ],
-      researchFields: ["Computational SOFC Modeling"]
+        "B.S., Incheon National University, 2026",
+      ],
+      researchFields: ["Computational SOFC Modeling"],
     },
   ],
   masterCourse: [
@@ -75,37 +88,41 @@ export const currentMembersData = {
       image: "/images/Memebers/CurrentMemebers/Master/Ms_Ye-eun Kim.png",
       career: [
         "M.S. Course at Incheon National University, 2023~",
-        "B.S., Incheon National University, 2023"
+        "B.S., Incheon National University, 2023",
       ],
-      researchFields: ["Solid Oxide Fuel Cells", "Exsolution", "Elecrospinning"]
+      researchFields: [
+        "Solid Oxide Fuel Cells",
+        "Exsolution",
+        "Elecrospinning",
+      ],
     },
     {
       name: "Somi Lee",
       image: "/images/Memebers/CurrentMemebers/Master/Ms_Somi Lee.jpg",
       career: [
         "M.S. Course at Incheon National University, 2023~",
-        "B.S., Incheon National University, 2023"
+        "B.S., Incheon National University, 2023",
       ],
-      researchFields: ["Solid Oxide Fuel Cells", "Sputtering"]
+      researchFields: ["Solid Oxide Fuel Cells", "Sputtering"],
     },
     {
       name: "DaHyun Han",
       image: "/images/Memebers/CurrentMemebers/Master/Ms_DaHyun Han.jpg",
       career: [
         "M.S. Course at Incheon National University, 2024~",
-        "B.S., Incheon National University, 2024"
+        "B.S., Incheon National University, 2024",
       ],
-      researchFields: ["Solid Oxide Fuel Cells", "Sputtering"]
+      researchFields: ["Solid Oxide Fuel Cells", "Sputtering"],
     },
-    {
-      name: "So Yeon Park",
-      image: "/images/Memebers/CurrentMemebers/Master/Ms_So Yeon Park.jpg",
-      career: [
-        "M.S. Course at Incheon National University, 2025~",
-        "B.S., Incheon National University, 2025"
-      ],
-      researchFields: ["Dry reforming of methane"]
-    },
+    // {
+    //   name: "So Yeon Park",
+    //   image: "/images/Memebers/CurrentMemebers/Master/Ms_So Yeon Park.jpg",
+    //   career: [
+    //     "M.S. Course at Incheon National University, 2025~",
+    //     "B.S., Incheon National University, 2025"
+    //   ],
+    //   researchFields: ["Dry reforming of methane"]
+    // },
   ],
   interns: [
     // {
@@ -116,13 +133,13 @@ export const currentMembersData = {
     //   ],
     //   researchFields: ["Protonic Ceramic Fuel Cells"]
     // },
-    {
-      name: "Soeun Jang",
-      image: "/images/Memebers/CurrentMemebers/Intern/Intern_Soeun Jang.jpg",
-      career: [
-        "Undergraduate Research Intern, 2025~"
-      ],
-      researchFields: ["Computational SOFC Modeling"]
-    }
-  ]
+    // {
+    //   name: "Soeun Jang",
+    //   image: "/images/Memebers/CurrentMemebers/Intern/Intern_Soeun Jang.jpg",
+    //   career: [
+    //     "Undergraduate Research Intern, 2025~"
+    //   ],
+    //   researchFields: ["Computational SOFC Modeling"]
+    // }
+  ],
 };
